12c6847c72d308d009b555b20c1f4fd06e443852
zhuyasen <zhuyasen@126.com>
2021-05-25 18:01:10 +0800

    modify render

---
+++ render/auth.go
@@ -0,0 +1,69 @@
+package render
+
+import (
+	"monitor-api/pkg/errcode"
+	"monitor-api/pkg/gohttp"
+	"monitor-api/pkg/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+// AuthResp 鉴权返回值
+type AuthResp struct {
+	User struct {
+		CustomerID int64  `json:"customerId"`
+		Enabled    bool   `json:"enabled"`
+		Username   string `json:"username"`
+		NickName   string `json:"nickName"`
+
+		//Roles        []struct {
+		//	ID        int64  `json:"id"`
+		//	DataScope string `json:"dataScope"`
+		//	Level     int64  `json:"level"`
+		//	Name      string `json:"name"`
+		//} `json:"roles"`
+		//Jobs       []struct {
+		//	ID   int64  `json:"id"`
+		//	Name string `json:"name"`
+		//} `json:"jobs"`
+	} `json:"user"`
+}
+
+// 判断是否通过鉴权
+func (a *AuthResp) IsAuth() bool {
+	if !a.User.Enabled || a.User.CustomerID <= 0 || a.User.Username == "" {
+		return false
+	}
+
+	return true
+}
+
+// CheckAuth 检查是否已鉴权
+func CheckAuth(url string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		result := &AuthResp{}
+
+		err := gohttp.GetJSON(result, url, nil, "Authorization", token)
+		if err != nil || !result.IsAuth() {
+			logger.Warn("check auth error", logger.String("url", url), logger.String("Authorization", token), logger.Err(err))
+			Error(c, errcode.UnauthorizedTokenError)
+			c.Abort()
+			return
+		}
+		c.Set("customerID", result.User.CustomerID)
+		c.Set("username", result.User.Username)
+		//  处理请求
+		c.Next()
+	}
+}
+
+// DefaultAuth 默认鉴权id
+func DefaultAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("customerID", 22)
+		c.Set("username", "admin")
+		//  处理请求
+		c.Next()
+	}
+}
---
+++ render/cors.go
@@ -0,0 +1,22 @@
+package render
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// 跨域
+func Cors() gin.HandlerFunc {
+	return cors.New(
+		cors.Config{
+			AllowAllOrigins:  true,
+			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+			AllowHeaders:     []string{"*"},
+			ExposeHeaders:    []string{"Content-Length", "text/plain", "Authorization", "Content-Type"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		},
+	)
+}
