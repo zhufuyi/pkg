37e57d1ef9703648d873e32adcfd5adaac0dfa3c
zhuyasen <zhuyasen@126.com>
2022-06-15 18:46:07 +0800

    add validator crud examples

--- gin/validator/README.md
+++ gin/validator/README.md
@@ -12,24 +12,4 @@ gin请求参数校验。
 
 ## 使用
 
-```go
-type userForm struct {
-	Name  string `form:"name" binding:"required"`
-	Age   int    `form:"age" binding:"gte=0,lte=150"`
-	Email string `form:"email" binding:"email,required"`
-}
-
-func CreateUser(c *gin.Context) {
-    form := &userForm{}
-    err := c.ShouldBindJSON(form)
-	// ...
-}
-func main() {
-	r := gin.Default()
-	binding.Validator = validator.Gin()
-	
-	r.POST("/createuser",CreateUser)
-    // ...
-}
-```
-
+查看示例 [validator_test](validator_test.go)
--- gin/validator/validator_test.go
+++ gin/validator/validator_test.go
@@ -3,37 +3,29 @@ package validator
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 )
 
-type userForm struct {
-	Name  string `json:"name" form:"name" binding:"required"`
-	Age   int    `json:"age" form:"age" binding:"gte=0,lte=150"`
-	Email string `json:"email" form:"email" binding:"email,required"`
-}
-
 func init() {
 	r := gin.Default()
 	binding.Validator = Gin()
 
-	helloFun := func(c *gin.Context) {
-		form := &userForm{}
-		err := c.ShouldBindJSON(form)
-		if err != nil {
-			fmt.Println(err)
-			c.JSON(400, "params is invalid")
-			return
-		}
-		c.JSON(200, "hello world")
-	}
-
-	r.POST("/hello", helloFun)
+	r.POST("/hello", createHello)
+	r.DELETE("/hello", deleteHello)
+	r.PUT("/hello", updateHello)
+	r.GET("/hello", getHello)
+	r.GET("/hello/:id", getHello)
+	r.GET("/hellos", getHellos)
 
 	go func() {
 		err := r.Run(":8080")
@@ -43,29 +35,108 @@ func init() {
 	}()
 }
 
-// -------------------------------------------------------------------------------------------
+var (
+	helloStr = "hello world"
+	paramErr = "params is invalid"
 
-func post(url string, body interface{}) ([]byte, error) {
-	v, err := json.Marshal(body)
-	if err != nil {
-		return nil, err
-	}
+	wantHello    = fmt.Sprintf(`"%s"`, helloStr)
+	wantParamErr = fmt.Sprintf(`"%s"`, paramErr)
+)
 
-	resp, err := http.Post(url, "application/json", bytes.NewReader(v))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	return ioutil.ReadAll(resp.Body)
+type postForm struct {
+	Name  string `json:"name" form:"name" binding:"required"`
+	Age   int    `json:"age" form:"age" binding:"gte=0,lte=150"`
+	Email string `json:"email" form:"email" binding:"email,required"`
 }
 
-func TestValidate(t *testing.T) {
-	wantHello := `"hello world"`
-	wantParamErr := `"params is invalid"`
+func createHello(c *gin.Context) {
+	form := &postForm{}
+	err := c.ShouldBindJSON(form)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, paramErr)
+		return
+	}
+	fmt.Printf("%+v\n", form)
+	c.JSON(http.StatusOK, helloStr)
+}
 
+type deleteForm struct {
+	IDS []uint64 `form:"ids" binding:"required,min=1"`
+}
+
+func deleteHello(c *gin.Context) {
+	form := &deleteForm{}
+	err := c.ShouldBindJSON(form)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, paramErr)
+		return
+	}
+	fmt.Printf("%+v\n", form)
+	c.JSON(http.StatusOK, helloStr)
+}
+
+type updateForm struct {
+	ID    uint64 `json:"id" form:"id" binding:"required,gt=0"`
+	Age   int    `json:"age" form:"age" binding:"gte=0,lte=150"`
+	Email string `json:"email" form:"email" binding:"email,required"`
+}
+
+func updateHello(c *gin.Context) {
+	form := &updateForm{}
+	err := c.ShouldBindJSON(form)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, paramErr)
+		return
+	}
+	fmt.Printf("%+v\n", form)
+	c.JSON(http.StatusOK, helloStr)
+}
+
+type getForm struct {
+	ID uint64 `form:"id" binding:"gt=0"`
+}
+
+func getHello(c *gin.Context) {
+	id, _ := strconv.ParseUint(c.Param("id"), 10, 0)
+	form := &getForm{ID: id}
+	err := c.ShouldBindQuery(form)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, paramErr)
+		return
+	}
+	fmt.Printf("%+v\n", form)
+	c.JSON(http.StatusOK, helloStr)
+}
+
+type getsForm struct {
+	Page int    `form:"page" binding:"gte=0"`
+	Size int    `form:"size" binding:"gt=0"`
+	Sort string `form:"sort" binding:"required,min=2"`
+}
+
+func getHellos(c *gin.Context) {
+	form := &getsForm{}
+	err := c.ShouldBindQuery(form)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, paramErr)
+		return
+	}
+	fmt.Printf("%+v\n", form)
+	c.JSON(http.StatusOK, helloStr)
+}
+
+// ------------------------------------------------------------------------------------------
+// http client
+// ------------------------------------------------------------------------------------------
+
+func TestPostValidate(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
-		got, err := post("http://localhost:8080/hello", &userForm{
+		got, err := do(http.MethodPost, "http://localhost:8080/hello", &postForm{
 			Name:  "foo",
 			Age:   10,
 			Email: "bar@gmail.com",
@@ -75,13 +146,12 @@ func TestValidate(t *testing.T) {
 			return
 		}
 		if string(got) != wantHello {
-
 			t.Errorf("got: %s, want: %s", got, wantHello)
 		}
 	})
 
 	t.Run("missing field error", func(t *testing.T) {
-		got, err := post("http://localhost:8080/hello", &userForm{
+		got, err := do(http.MethodPost, "http://localhost:8080/hello", &postForm{
 			Age:   10,
 			Email: "bar@gmail.com",
 		})
@@ -95,7 +165,7 @@ func TestValidate(t *testing.T) {
 	})
 
 	t.Run("field range  error", func(t *testing.T) {
-		got, err := post("http://localhost:8080/hello", &userForm{
+		got, err := do(http.MethodPost, "http://localhost:8080/hello", &postForm{
 			Name:  "foo",
 			Age:   -1,
 			Email: "bar@gmail.com",
@@ -110,9 +180,9 @@ func TestValidate(t *testing.T) {
 	})
 
 	t.Run("email error", func(t *testing.T) {
-		got, err := post("http://localhost:8080/hello", &userForm{
+		got, err := do(http.MethodPost, "http://localhost:8080/hello", &postForm{
 			Name:  "foo",
-			Age:   -10,
+			Age:   10,
 			Email: "bar",
 		})
 		if err != nil {
@@ -123,4 +193,207 @@ func TestValidate(t *testing.T) {
 			t.Errorf("got: %s, want: %s", got, wantParamErr)
 		}
 	})
+}
+
+// ------------------------------------------------------------------------------------------
+
+func TestDeleteValidate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		got, err := do(http.MethodDelete, "http://localhost:8080/hello", &deleteForm{
+			IDS: []uint64{1, 2, 3},
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantHello {
+			t.Errorf("got: %s, want: %s", got, wantHello)
+		}
+	})
+
+	t.Run("missing field error", func(t *testing.T) {
+		got, err := do(http.MethodDelete, "http://localhost:8080/hello", nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantParamErr {
+			t.Errorf("got: %s, want: %s", got, wantParamErr)
+		}
+	})
+
+	t.Run("ids  error", func(t *testing.T) {
+		got, err := do(http.MethodDelete, "http://localhost:8080/hello", &deleteForm{IDS: []uint64{}})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantParamErr {
+			t.Errorf("got: %s, want: %s", got, wantParamErr)
+		}
+	})
+}
+
+// -------------------------------------------------------------------------------------------
+
+func TestPutValidate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		got, err := do(http.MethodPut, "http://localhost:8080/hello", &updateForm{
+			ID:    100,
+			Age:   10,
+			Email: "bar@gmail.com",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantHello {
+			t.Errorf("got: %s, want: %s", got, wantHello)
+		}
+	})
+
+	t.Run("missing field error", func(t *testing.T) {
+		got, err := do(http.MethodPut, "http://localhost:8080/hello", &updateForm{
+			Age:   10,
+			Email: "bar@gmail.com",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantParamErr {
+			t.Errorf("got: %s, want: %s", got, wantParamErr)
+		}
+	})
+
+	t.Run("email error", func(t *testing.T) {
+		got, err := do(http.MethodPut, "http://localhost:8080/hello", &updateForm{
+			ID:    101,
+			Age:   10,
+			Email: "bar",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantParamErr {
+			t.Errorf("got: %s, want: %s", got, wantParamErr)
+		}
+	})
+}
+
+// -------------------------------------------------------------------------------------------
+
+func TestGetValidate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		got, err := do(http.MethodGet, "http://localhost:8080/hello?id=100", nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantHello {
+			t.Errorf("got: %s, want: %s", got, wantHello)
+		}
+	})
+
+	t.Run("success2", func(t *testing.T) {
+		got, err := do(http.MethodGet, "http://localhost:8080/hello/101", nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantHello {
+			t.Errorf("got: %s, want: %s", got, wantHello)
+		}
+	})
+
+	t.Run("miss id error", func(t *testing.T) {
+		got, err := do(http.MethodGet, "http://localhost:8080/hello", nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantParamErr {
+			t.Errorf("got: %s, want: %s", got, wantParamErr)
+		}
+	})
+}
+
+// -------------------------------------------------------------------------------------------
+
+func TestGetsValidate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		got, err := do(http.MethodGet, "http://localhost:8080/hellos?page=0&size=10&sort=-id", nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantHello {
+			t.Errorf("got: %s, want: %s", got, wantHello)
+		}
+	})
+
+	t.Run("missing field error", func(t *testing.T) {
+		got, err := do(http.MethodGet, "http://localhost:8080/hellos?page=0&size=10", nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantParamErr {
+			t.Errorf("got: %s, want: %s", got, wantParamErr)
+		}
+	})
+
+	t.Run("size error", func(t *testing.T) {
+		got, err := do(http.MethodGet, "http://localhost:8080/hellos?page=0&size=0&sort=-id", nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if string(got) != wantParamErr {
+			t.Errorf("got: %s, want: %s", got, wantParamErr)
+		}
+	})
+}
+
+// ------------------------------------------------------------------------------------------
+
+func do(method string, url string, body interface{}) ([]byte, error) {
+	var reader io.Reader
+	if body == nil {
+		reader = nil
+	} else {
+		v, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		reader = bytes.NewReader(v)
+	}
+
+	method = strings.ToUpper(method)
+	switch method {
+	case http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete:
+		req, err := http.NewRequest(method, url, reader)
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Add("Content-Type", "application/json")
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		return ioutil.ReadAll(resp.Body)
+
+	case http.MethodGet:
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		return ioutil.ReadAll(resp.Body)
+
+	default:
+		return nil, errors.New("unknown method")
+	}
 }
